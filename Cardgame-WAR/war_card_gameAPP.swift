cca2981233aacd5cc4cf005b913b0a8bee737312
Daniel Higgins <dannyhiggs5@gmail.com>
2023-07-19 17:33:25 -0400

    Create war_card_gameAPP.swift

---
+++ Cardgame-WAR/war_card_gameAPP.swift
@@ -0,0 +1,17 @@
+//
+//  war_card_gameApp.swift
+//  war card game
+// details provided by CodeWithChris on youtube
+//  Created by DANIEL HIGGINS on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct war_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
